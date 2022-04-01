e123e32a7bd977068c09a9b768edf4052c1389a5
Mats Roshauw <karmats@gmail.com>
2022-04-01 21:07:37 +0200

    Lnu - laboration 2 WIP

---
+++ lnu/U1/L2/js/script.js
@@ -0,0 +1,14 @@
+// Globala variabler
+
+
+// ------------------------------
+// Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd.
+// Initiering av globala variabler samt koppling av funktioner till knapparna.
+function init() {
+	
+} // End init
+window.onload = init; // Se till att init aktiveras då sidan är inladdad
+// ------------------------------
+
+
+
