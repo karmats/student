706ca83474dde918084d61e280aec3422b77434f
Mats Roshauw <karmats@gmail.com>
2022-05-11 20:00:14 +0200

    Lnu - Laboration 7

--- lnu/U2/L7/js/script7a.js
+++ lnu/U2/L7/js/script7a.js
@@ -1,18 +1,37 @@
 // Initiera globala variabler och koppla funktion till knapp
 function init() {
-	initBoxes();
-	document.getElementById("saveBtn").addEventListener("click",saveBoxPositions);
-	getBoxPositions();
+  initBoxes();
+  document
+    .getElementById("saveBtn")
+    .addEventListener("click", saveBoxPositions);
+  getBoxPositions();
 } // End init
-window.addEventListener("load",init);
+window.addEventListener("load", init);
 // ------------------------------
 // Spara boxarnas placering i en cookie
 function saveBoxPositions() {
-	
+  let posStr = "";
+  for (let i = 0; i < boxElems.length; i++) {
+    let { left, top } = boxElems[i].style;
+    posStr +=
+      "#" +
+      left.substring(0, left.length - 2) +
+      "," +
+      top.substring(0, top.length - 2);
+  }
+  setCookie("boxPos", posStr.substring(1), 30);
 } // End saveBoxPositions
 // ------------------------------
 // Hämta sprade positioner uppdatera boxarnas placering
 function getBoxPositions() {
-	
+  let posStr = getCookie("boxPos");
+  if (posStr) {
+    let posistions = posStr.split("#");
+    for (let i = 0; i < posistions.length; i++) {
+      let [left, top] = posistions[i].split(",");
+      boxElems[i].style.left = left + "px";
+      boxElems[i].style.top = top + "px";
+    }
+  }
 } // End getBoxPositions
 // ------------------------------
--- lnu/U2/L7/js/script7b.js
+++ lnu/U2/L7/js/script7b.js
@@ -1,8 +1,36 @@
 // Initiera globala variabler och koppla funktion till knapp
 function init() {
-	initBoxes();
-	document.getElementById("saveBtn").addEventListener("click",saveBoxPositions);
-	getBoxPositions();
+  initBoxes();
+  document
+    .getElementById("saveBtn")
+    .addEventListener("click", saveBoxPositions);
+  getBoxPositions();
 } // End init
-window.addEventListener("load",init);
+window.addEventListener("load", init);
+// ------------------------------
+// Spara boxarnas placering i localstorage
+function saveBoxPositions() {
+  let posStr = "";
+  for (let i = 0; i < boxElems.length; i++) {
+    let { left, top } = boxElems[i].style;
+    posStr +=
+      "#" +
+      left.substring(0, left.length - 2) +
+      "," +
+      top.substring(0, top.length - 2);
+  }
+  localStorage.boxPos = posStr.substring(1);
+} // End saveBoxPositions
+// ------------------------------
+// Hämta sprade positioner uppdatera boxarnas placering
+function getBoxPositions() {
+  if (localStorage.boxPos) {
+    let posistions = localStorage.boxPos.split("#");
+    for (let i = 0; i < posistions.length; i++) {
+      let [left, top] = posistions[i].split(",");
+      boxElems[i].style.left = left + "px";
+      boxElems[i].style.top = top + "px";
+    }
+  }
+} // End getBoxPositions
 // ------------------------------
